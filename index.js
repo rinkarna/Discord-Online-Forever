1bfefa3a461381d6b8c35df50ed2f96db6c5aa7b
rinkarna <agunggalihdeveloper@gmail.com>
2025-11-14 23:29:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const keep_alive = require('./keep_alive.js')
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
+bot.on("ready", () => {
+  console.log("Bot is ready!");
+  // Set the bot's presence/activity
+  bot.editStatus("online", {
+    name: "Roblox",
+    type: 0 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 4 = Custom, 5 = Competing
+  });
+});
+
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
